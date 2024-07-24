b5ddedae24f4976db0d0259f74b0ebd61d4113d3
abdohosny1 <make.y90@yahoo.com>
2024-07-24 11:57:20 +0300

    Add project files.

---
+++ Use Polly to make Resilient & Robust API/Controllers/ServicesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Polly;
+using RestSharp;
+using Use_Polly_to_make_Resilient___Robust_API.services;
+
+namespace Use_Polly_to_make_Resilient___Robust_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ILogger<ServicesController> _logger;
+        private readonly NorrisServices _norrisServices;
+
+        public ServicesController(ILogger<ServicesController> logger, NorrisServices norrisServices)
+        {
+            _logger = logger;
+            _norrisServices = norrisServices;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Get()
+        {
+
+            var amountToWait = TimeSpan.FromSeconds(5);
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(5, _ => amountToWait, (exception, retryCount, context) =>
+                {
+                    Console.WriteLine($"Error: {exception.Message} - Retry: {retryCount}");
+                });
+
+
+
+
+            var response = await retryPolicy.ExecuteAsync( async () => await _norrisServices.NorrisServicesAsync());
+
+
+
+                return Ok(response);
+         
+
+        }
+    }
+}
---
+++ Use Polly to make Resilient & Robust API/services/ChuckNorrisServices.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using RestSharp;
+
+namespace Use_Polly_to_make_Resilient___Robust_API.services
+{
+    public class NorrisServices
+    {
+        //public async Task<RestResponse> NorrisServicesAsync(CancellationToken cancellationToken = default)
+        //{
+        //    var url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
+
+        //    var client = new RestClient();
+        //    var request = new RestRequest(url, Method.Get);
+        //    request.AddHeader("x-rapidapi-key", "ac83b0fefcmsh4b87e02f273ac48p1052d9jsn57ec4c976b31");
+        //    request.AddHeader("x-rapidapi-host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
+        //    request.AddHeader("accept", "application/json");
+
+        //    var response = await client.ExecuteAsync(request);
+
+        //        return response;
+        
+        //}
+
+        public async Task<string> NorrisServicesAsync(CancellationToken cancellationToken = default)
+        {
+            var url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random";
+
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Get);
+            request.AddHeader("x-rapidapi-key", "ac83b0fefcmsh4b87e02f273ac48p1052d9jsn57ec4c976b31");
+            request.AddHeader("x-rapidapi-host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com");
+            request.AddHeader("accept", "application/json");
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                return response.Content;
+
+            }
+            else
+            {
+                throw new Exception($"{response.Content}");
+            }
+
+        }
+    }
+}
